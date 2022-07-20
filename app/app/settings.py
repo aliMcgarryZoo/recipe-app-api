6b3f1a1ea2d4090d482af2c6ad3e611fb047a93f
Ali McGarry <ali.mcgarry@zoodigital.com>
2022-07-20 14:38:46 +0100

    Added user app

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,9 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
     'rest_framework',
     'drf_spectacular',
+    'user',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -134,5 +135,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'
 
 REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoShema'
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
 }
